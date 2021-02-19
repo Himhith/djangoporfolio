a55c4ad0d57d95aa834b578070ac3c2beea756e6
Michał Ogorzałek <michal.ogorzalek@gmail.com>
2021-02-19 15:46:39 +0100

    minor

--- djangoportfo/settings.py
+++ djangoportfo/settings.py
@@ -68,7 +68,8 @@ ROOT_URLCONF = 'djangoportfo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR, "templates"],
+# deafault path 'djangoportfo\\templates\\'
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
